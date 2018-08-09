e5ebaa534b580746c1ec371ec51d23182b0ded54
Morri1eu <morri1eu@gmail.com>
2018-08-09 09:26:07 -0700

    added code for deployment to heroku

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var exphbs = require('express-handlebars');
 
 var bodyParser = require('body-parser')
 
-var db= require("./models")
+var db = require("./models")
 var app = express();
 
 // parse application/x-www-form-urlencoded
@@ -13,7 +13,7 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
 
-var PORT = 3000;
+var PORT = process.env.PORT ||3000;
 //scraping tools
 var request = require('request');
 const cheerio = require('cheerio')
@@ -25,7 +25,9 @@ app.use(express.static("public"));
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
-mongoose.connect('mongodb://localhost:27017/NYtimes', { useNewUrlParser: true })
+const MONGODBURL = process.env.MONGODB_URI || "mongodb://localhost:27017/NYtimes";
+
+mongoose.connect(MONGODBURL, { useNewUrlParser: true });
 //home route
 app.get('/', function (req, res) {
     request.get('https://www.nytimes.com/', function (error, respose, body) {
@@ -36,26 +38,27 @@ app.get('/', function (req, res) {
             article.title = $(this).children("h2.story-heading").children("a").text()
             article.link = $(this).children("h2.story-heading").children("a").attr("href")
             article.summary = $(this).children("p.summary").text()
-            
-            if (article.title.length>1&&article.link.length>1&&article.summary.length>1){
-                db.Article.findOne({title:article.title}).then(function(result){
+
+            if (article.title.length > 1 && article.link.length > 1 && article.summary.length > 1) {
+                db.Article.findOne({ title: article.title }).then(function (result) {
                     x = result
                     console.log(x)
                 })
-                if(x===undefined){
-            db.Article.create(article)
-                .then(function (createdArticle) {
-                    console.log(createdArticle)
-                })
-                .catch(function (err) {
-                    console.log(err)
-                })}
-                else{}
+                if (x === undefined) {
+                    db.Article.create(article)
+                        .then(function (createdArticle) {
+                            console.log(createdArticle)
+                        })
+                        .catch(function (err) {
+                            console.log(err)
+                        })
+                }
+                else { }
             }
-            else{}
+            else { }
         })
         db.Article.find({}).then(function (data) {
-            console.log("-----------------"+data)
+            console.log("-----------------" + data)
             res.render("home", { Articles: data })
         })
     })
@@ -63,19 +66,19 @@ app.get('/', function (req, res) {
 
 });
 
-app.get("/saved", function(req,res){
-    db.Article.find({saved:true}).then(result=> res.render("saved", {Articles:result}))
-    
+app.get("/saved", function (req, res) {
+    db.Article.find({ saved: true }).then(result => res.render("saved", { Articles: result }))
+
 })
 
-app.put("/api/:id", function(req,res){
+app.put("/api/:id", function (req, res) {
     console.log(req.body)
-    db.Article.findByIdAndUpdate(req.params.id, req.body, function(err, res){
-    }).then(result=> res.json(result))
+    db.Article.findByIdAndUpdate(req.params.id, req.body, function (err, res) {
+    }).then(result => res.json(result))
 })
 
-app.delete("/api", function(req,res){
-    db.Article.deleteMany({},function(result){
+app.delete("/api", function (req, res) {
+    db.Article.deleteMany({}, function (result) {
         res.json(result)
     })
 })
